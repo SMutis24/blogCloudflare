be4cbd353e8df6ac470d42dcf56e2007c04e19ae
Santiago Mutis <santiagomutis24020@gmail.com>
2025-05-06 22:06:31 -0500

    fix: :bug: Validar error esta raro

---
+++ db/config.ts
@@ -0,0 +1,13 @@
+import { column, defineDb, defineTable } from "astro:db";
+
+// https://astro.build/db/config
+const Comment = defineTable({
+  columns: {
+    author: column.text(),
+    body: column.text(),
+  },
+});
+
+export default defineDb({
+  tables: { Comment },
+});
