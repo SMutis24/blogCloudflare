ba4f1aa19a4c24999918f04aff37c185b6476d7d
Santiago Mutis <santiagomutis24020@gmail.com>
2025-05-08 21:49:59 -0500

    feat:

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
